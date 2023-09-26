f1cb570c0bb5ce4e6f619e602a41a87fbea3ff86
Gregor Wolf <gregor.wolf@gmail.com>
2023-09-26 09:05:38 +0200

    fix read issues

--- srv/catalog-service.js
+++ srv/catalog-service.js
@@ -14,30 +14,32 @@ module.exports = cds.service.impl(async (srv) => {
       db.run(DELETE.from(Details));
       const filter =
         "$filter=(substringof('$SAP Fiori (SAPUI5)$',UITechnologyCombined) or substringof('$SAP Fiori app variant$',UITechnologyCombined) or substringof('$SAP Fiori elements$',UITechnologyCombined) or substringof('$SAP Fiori elements for CoPilot$',UITechnologyCombined) or substringof('$SAP Fiori elements: Overview Page$',UITechnologyCombined) or substringof('$SAP Fiori: Analysis Path Framework (APF)$',UITechnologyCombined) or substringof('$SAP Fiori: Design Studio$',UITechnologyCombined) or substringof('$SAP Fiori: Generic Application Log Framework$',UITechnologyCombined) or substringof('$SAP Fiori: Generic Configuration Framework$',UITechnologyCombined) or substringof('$SAP Fiori: Generic Job Scheduling Framework$',UITechnologyCombined) or substringof('$SAP Fiori: Manage Workflow$',UITechnologyCombined) or substringof('$SAP Fiori: My Inbox$',UITechnologyCombined) or substringof('$SAP Fiori: SAP Smart Business generic drill down app$',UITechnologyCombined) or substringof('$SAP Fiori: SAP Smart Business tile %26 custom drill down app$',UITechnologyCombined))";
-      const appsCount = await fioriAppsLibrary
-        .tx(req)
-        .get(
-          "/InputFilterParam(InpFilterValue='1NA')/Results/$count" +
-            "?" +
-            filter
-        );
+      const appsCount = await fioriAppsLibrary.send({
+        query:
+          "GET InputFilterParam(InpFilterValue='1NA')/Results/$count" +
+          "?" +
+          filter,
+      });
       console.log("There are ", appsCount, " matching Apps");
-      const apps = await fioriAppsLibrary.tx(req).get(
-        "/InputFilterParam(InpFilterValue='1NA')/Results" +
+      const apps = await fioriAppsLibrary.send({
+        query:
+          "InputFilterParam(InpFilterValue='1NA')/Results" +
           "?" +
           filter +
-          "&$select=appId,releaseId,AppNameAll"
+          "&$select=appId,releaseId,AppNameAll",
         // + "&$top=10"
-      );
+      });
       console.log("Number of result for detail read: ", apps.length);
       for (let index = 0; index < apps.length; index++) {
         const app = apps[index];
         const insertDetailsResult = await db.run(
-          INSERT.into(Details).rows({
-            fioriId: app.appId,
-            releaseId: app.releaseId,
-            AppName: app.AppNameAll,
-          })
+          INSERT.into(Details).rows([
+            {
+              fioriId: app.appId,
+              releaseId: app.releaseId,
+              AppName: app.AppNameAll,
+            },
+          ])
         );
       }
       console.log("Load finished");
