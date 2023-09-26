ee8a80f4851c6b83cac5b204a0ca6a695074854f
Gregor Wolf <gregor.wolf@gmail.com>
2023-09-26 08:09:37 +0200

    comment inserts to fix start issue for the moment

--- srv/init.js
+++ srv/init.js
@@ -7,13 +7,14 @@ RequiredODataServicessData.d.results.forEach(cleanObject);
 
 module.exports = (db) => {
   const { Details, RequiredODataServices } = db.entities;
-
   return cds.run([
     DELETE.from(Details).where("1 = 1"),
-    INSERT.into(Details).rows(DetailsData.d.results),
+    //    INSERT.into(Details).rows(DetailsData.d.results),
     DELETE.from(RequiredODataServices).where("1 = 1"),
+    /*
     INSERT.into(RequiredODataServices).rows(
       RequiredODataServicessData.d.results
     ),
+    */
   ]);
 };
